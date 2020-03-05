1de5f0e72f614967b353a60b4cf03e8a872a2f5c
Brandon Scott <Brandon.Scott@capstonec.com>
2020-03-05 14:03:54 -0600

    Adding projects

---
+++ GpcRpcClient/GpcRpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+
+namespace GpcRpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "Brandon" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
